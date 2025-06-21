3982ed162b6ea90495255b28f148c03b5fccd690
tima-hu <Email@.com>
2025-06-21 15:45:27 +0600

    дз2

--- ра/ро.py
+++ ра/ро.py
@@ -1,30 +1,29 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
-from PyQt6.QtCore import Qt  
+from PyQt6.QtCore import Qt
 
 class ExerciseApp(QWidget):
-    def __init__(self):  
-        super().__init__()
+    def init(self):
+        super().init()
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle("Приложение-упражнение")
         self.setFixedSize(400, 300)
 
+        
         self.label = QLabel("Ожидаю действий...", self)
-        self.label.setWordWrap(True)
+        self.label.setWordWrap(True)  
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
+        
         self.hello_btn = QPushButton("Привет")
         self.bye_btn = QPushButton("Пока")
-
         self.hello_btn.clicked.connect(self.say_hello)
         self.bye_btn.clicked.connect(self.say_bye)
-
         button_layout = QHBoxLayout()
         button_layout.addWidget(self.hello_btn)
         button_layout.addWidget(self.bye_btn)
-
         main_layout = QVBoxLayout()
         main_layout.addWidget(self.label)
         main_layout.addLayout(button_layout)
